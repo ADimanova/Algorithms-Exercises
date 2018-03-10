3d3992bd39713494c876040ceff4e4a15dd7a0c9
ADimanova <addidimanova@gmail.com>
2018-03-10 13:38:58 +0200

    Update ClosestSumPair.cs

--- Search/ClosestSumPair.cs
+++ Search/ClosestSumPair.cs
@@ -5,8 +5,15 @@ using System.Linq;
 class Solution {
     static void Main(String[] args) {
         //Given a sorted array and a number x, find a pair in array whose sum is closest to x.
-        var arr = new int[] {2, 3, 15, 18, 23, 25, 30};
-        var expectedSum = 44;
+        
+        //var arr = new int[] {2, 3, 15, 18, 23, 25, 30};
+        //var expectedSum = 44;
+        
+        //var arr = new int[] {1, 3, 5, 6, 8, 12, 15, 18, 22, 44};
+        //var expectedSum = 20;
+        
+        var arr = new int[] {1, 4, 7, 10};
+        var expectedSum = 15;
         
         var bestSum = 0;
         var i = 0;
@@ -18,18 +25,28 @@ class Solution {
             var newSum = arr[i] + arr[j];
 
             if(expectedSum == newSum)
+            {
+                bestSum = newSum;
+                bestI = i;
+                bestJ = j;
                 break;
-            if(expectedSum > newSum)
-                i++;
-            if(expectedSum < newSum)
-                j--;
+            }
 
             if(Math.Abs(expectedSum - newSum) < Math.Abs(expectedSum - bestSum))
             {
                 bestSum = newSum;
                 bestI = i;
                 bestJ = j;
-            }     
+            }
+            
+            if(expectedSum > newSum)
+            {
+                i++;                
+            }
+            else if(expectedSum < newSum)
+            {
+                j--;                
+            }
         }
         
         Console.WriteLine(
