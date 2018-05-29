ce1a69d78073eee96c46999dfb077a92482beb1e
Ilian Trifonov <shogun11@abv.bg>
2018-05-29 20:28:58 +0300

    Create FirstBadVersion.cs

---
+++ Search/FirstBadVersion.cs
@@ -0,0 +1,35 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    /*
+    You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+    You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+    */
+    public int FirstBadVersion(int n) {
+        return BinarySearch(1, n);
+    }
+    
+    public int BinarySearch(int left, int right){
+        var mid = left;
+        while(left <= right){
+            mid = left + ((right - left) / 2);
+            if(IsBadVersion(mid)){
+                if(mid == left || !IsBadVersion(mid - 1)){
+                    break;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+            else {
+                left  = mid + 1;
+            }
+        }
+        
+        return mid;
+    }
+}
